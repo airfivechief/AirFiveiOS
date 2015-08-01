a2423a3ac64242548e6bc71b9dc62901e01e1500
Josh Gafni <jgafni@gmail.com>
2015-07-31 18:23:27 -0700

    Add Keyboard Show/Hide (commented out for now)

--- AirFiveiOS/AFCardViewController.m
+++ AirFiveiOS/AFCardViewController.m
@@ -15,10 +15,13 @@
 @property (weak, nonatomic) IBOutlet UITextField *positionAndOrgTextField;
 @property (weak, nonatomic) IBOutlet UITextField *industryTextField;
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
 @property (weak, nonatomic) IBOutlet UITextField *websiteTextField;
 @property (weak, nonatomic) IBOutlet UITextField *recipientEmailTextField;
 
 @property (strong, nonatomic) NSString *recipientEmailAddress;
+@property (weak, nonatomic) UITextField *selectedTextField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *logoVerticalConstraint;
 
 @end
 
@@ -39,6 +42,14 @@
 {
     [super viewWillAppear:animated];
     [self loadCard];
+    [self registerForKeyboardNotifications];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self resignFirstResponder];
+    [self deregisterFromKeyboardNotifications];
 }
 
 - (void)loadCard
@@ -47,7 +58,9 @@
     self.positionAndOrgTextField.text = [NSString stringWithFormat:@"%@, %@", self.position, self.organization];
     self.industryTextField.text = self.industry;
     self.emailTextField.text = self.emailAddress;
+    self.phoneTextField.text = self.phone;
     self.websiteTextField.text = self.website;
+    
 }
 
 - (IBAction)shareButtonTouched:(UIButton *)sender
@@ -58,6 +71,7 @@
     [AFEmailManager sharedInstance].organization = self.organization;
     [AFEmailManager sharedInstance].industry = self.industry;
     [AFEmailManager sharedInstance].emailAddress = self.emailAddress;
+    [AFEmailManager sharedInstance].phone = self.phone;
     [AFEmailManager sharedInstance].website = self.website;
     [AFEmailManager sharedInstance].recipientEmailAddress = self.recipientEmailAddress;;
     [[AFEmailManager sharedInstance] sendEmail];
@@ -68,6 +82,7 @@
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
+    self.selectedTextField = textField;
     NSString *text = [textField.text stringByReplacingCharactersInRange:range withString:string];
     if(textField == self.fullNameTextField){
         self.fullName = text;
@@ -81,6 +96,16 @@
     else if(textField == self.emailTextField){
         self.emailAddress = text;
     }
+    else if(textField == self.phoneTextField){
+        if (range.length == 1) {
+            // Delete button was hit.. so tell the method to delete the last char.
+            textField.text = [self formatPhoneNumber:text deleteLastChar:YES];
+        } else {
+            textField.text = [self formatPhoneNumber:text deleteLastChar:NO ];
+        }
+        self.phone = textField.text;
+        return NO;
+    }
     else if(textField == self.websiteTextField){
         self.website = text;
     }
@@ -90,4 +115,86 @@
     return YES;
 }
 
+//http://stackoverflow.com/questions/1246439/uitextfield-for-phone-number
+-(NSString*) formatPhoneNumber:(NSString*) simpleNumber deleteLastChar:(BOOL)deleteLastChar {
+    if(simpleNumber.length==0) return @"";
+    // use regex to remove non-digits(including spaces) so we are left with just the numbers
+    NSError *error = NULL;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[\\s-\\(\\)]" options:NSRegularExpressionCaseInsensitive error:&error];
+    simpleNumber = [regex stringByReplacingMatchesInString:simpleNumber options:0 range:NSMakeRange(0, [simpleNumber length]) withTemplate:@""];
+    
+    // check if the number is to long
+    if(simpleNumber.length>10) {
+        // remove last extra chars.
+        simpleNumber = [simpleNumber substringToIndex:10];
+    }
+    
+    if(deleteLastChar) {
+        // should we delete the last digit?
+        simpleNumber = [simpleNumber substringToIndex:[simpleNumber length] - 1];
+    }
+    
+    // 123 456 7890
+    // format the number.. if it's less then 7 digits.. then use this regex.
+    if(simpleNumber.length<7)
+        simpleNumber = [simpleNumber stringByReplacingOccurrencesOfString:@"(\\d{3})(\\d+)"
+                                                               withString:@"($1) $2"
+                                                                  options:NSRegularExpressionSearch
+                                                                    range:NSMakeRange(0, [simpleNumber length])];
+    
+    else   // else do this one..
+        simpleNumber = [simpleNumber stringByReplacingOccurrencesOfString:@"(\\d{3})(\\d{3})(\\d+)"
+                                                               withString:@"($1) $2-$3"
+                                                                  options:NSRegularExpressionSearch
+                                                                    range:NSMakeRange(0, [simpleNumber length])];
+    return simpleNumber;
+}
+
+#pragma mark - Keyboard
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)deregisterFromKeyboardNotifications {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+    [UIView setAnimationCurve:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    int keyboardHeight = MIN(keyboardSize.height,keyboardSize.width);
+    //self.logoVerticalConstraint.constant = 40 - keyboardHeight;
+    [self.view layoutIfNeeded];
+    [UIView commitAnimations];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+    [UIView setAnimationCurve:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    //self.logoVerticalConstraint.constant = 40;
+    [self.view layoutIfNeeded];
+    [UIView commitAnimations];
+}
+
+- (IBAction)tapGestureRecognizer:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+}
 @end
