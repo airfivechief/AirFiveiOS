16621860317e83aa05175dcf4c22861d8f447602
Josh Gafni <jgafni@gmail.com>
2015-08-03 02:27:47 -0700

    Separate Card View Controller from Share View Controller

---
+++ AirFiveiOS/AFShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFShareViewController.h
+//  AirFiveiOS
+//
+//  Created by Joshua Gafni on 8/3/15.
+//  Copyright (c) 2015 AirFive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFShareViewController : UIViewController
+
+@end
---
+++ AirFiveiOS/AFShareViewController.m
@@ -0,0 +1,13 @@
+//
+//  AFShareViewController.m
+//  AirFiveiOS
+//
+//  Created by Joshua Gafni on 8/3/15.
+//  Copyright (c) 2015 AirFive. All rights reserved.
+//
+
+#import "AFShareViewController.h"
+
+@implementation AFShareViewController
+
+@end
