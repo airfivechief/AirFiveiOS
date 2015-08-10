179290b81a0d203c3ed4540ddb5c07c30ed27f6f
Josh Gafni <jgafni@gmail.com>
2015-08-10 00:13:42 -0700

    Update Style of Navigation View

---
+++ AirFiveiOS/UIUnderlinedButton.h
@@ -0,0 +1,13 @@
+//
+//  UIUnderlinedButton.h
+//  AirFiveiOS
+//
+//  Created by Joshua Gafni on 8/10/15.
+//  Copyright (c) 2015 AirFive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIUnderlinedButton : UIButton
+
+@end
---
+++ AirFiveiOS/UIUnderlinedButton.m
@@ -0,0 +1,13 @@
+//
+//  UIUnderlinedButton.m
+//  AirFiveiOS
+//
+//  Created by Joshua Gafni on 8/10/15.
+//  Copyright (c) 2015 AirFive. All rights reserved.
+//
+
+#import "UIUnderlinedButton.h"
+
+@implementation UIUnderlinedButton
+
+@end
